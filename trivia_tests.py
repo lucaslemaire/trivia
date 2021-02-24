a0a605643eb254677d005f965681a0871c8f9842
Arbouille <m.arbouille@gmail.com>
2021-02-24 10:15:32 +0100

    Tests

--- trivia_tests.py
+++ trivia_tests.py
@@ -3,11 +3,11 @@ from trivia import Game
 def test_number_of_players_more_than_one():
     game = Game()
     game.add('P1')
-
+    
     assert game.is_playable() == False
 
-    game.add('P2')
-
+    game.add('P2')   
+    
     assert game.is_playable() == True
 
 
@@ -20,3 +20,19 @@ def test_number_of_players_less_than_seven():
     game.add('P5')
     assert game.add('P6') == True
     assert game.add('P7') == False
+
+def add_player():
+    game = Game()
+    game.add('P1')
+    game.current_player == 1
+    game.add('P2')
+    game.current_player == 2
+    
+def leave_game():
+    game = Game()
+    game.add('P1')
+    game.add('P2')
+    game.add('P3')
+    game.players.remove(game.player)
+    game.current_player == 2
+
